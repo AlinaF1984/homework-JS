56164ce8b4e3e77b5b8f3ae5613a7428bc66ccea
AlinaF1984 <afassahova9@gmail.com>
2023-01-31 17:09:39 +0500

    task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+if (arr1.length === arr2.length && arr1.every((element, i) => element === arr2[i])) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(users => users.gender === gender).reduce((acc, item, idx, arr) => acc + item.age / arr.length, 0); 
+    return result;
 }
+  
